cc4077cfed21c488518e2a2d593d5cbd48cb3c4b
Oscar Fernandez <oscarferm13@gmail.com>
2025-06-09 15:28:04 +0200

    Filter 3-dots menu to insert 'CreateButton'

--- src/content.jsx
+++ src/content.jsx
@@ -30,18 +30,27 @@ function observeMenuOpen() {
   const menuSelector = '[role="menu"][data-radix-menu-content]';
   const menuItemClass = 'touch:min-h-10 group relative mx-1.5 my-0 flex can-hover:cursor-pointer items-center rounded-[10px] py-2 px-2.5 text-sm select-none';
 
-  const observer = new MutationObserver(() => {
-    const menu = document.querySelector(menuSelector);
-    if (menu && !menu.querySelector('.custom-menu-item')) {
-      const newItem = document.createElement('div');
-      newItem.className = menuItemClass + ' custom-menu-item';
-      newItem.role = 'menuitem';
-      
-      menu.prepend(newItem);
+  const targetPathD =
+    "M13.2929 4.29291C15.0641 2.52167 17.9359 2.52167 19.7071 4.2929C21.4784 6.06414 21.4784 8.93588 19.7071 10.7071L18.7073 11.7069L11.6135 18.8007C10.8766 19.5376 9.92793 20.0258 8.89999 20.1971L4.16441 20.9864C3.84585 21.0395 3.52127 20.9355 3.29291 20.7071C3.06454 20.4788 2.96053 20.1542 3.01362 19.8356L3.80288 15.1C3.9742 14.0721 4.46243 13.1234 5.19932 12.3865L13.2929 4.29291ZM13 7.41422L6.61353 13.8007C6.1714 14.2428 5.87846 14.8121 5.77567 15.4288L5.21656 18.7835L8.57119 18.2244C9.18795 18.1216 9.75719 17.8286 10.1993 17.3865L16.5858 11L13 7.41422ZM18 9.5858L14.4142 6.00001L14.7071 5.70712C15.6973 4.71693 17.3027 4.71693 18.2929 5.70712C19.2831 6.69731 19.2831 8.30272 18.2929 9.29291L18 9.5858Z";
 
-      const root = createRoot(newItem);
-      root.render(<CreateButton />);
-    }
+  const observer = new MutationObserver(() => {
+    const menus = document.querySelectorAll(menuSelector);
+
+    menus.forEach((menu) => {
+      const hasTargetPath = menu.querySelector(`path[d="${targetPathD}"]`);
+      const alreadyInjected = menu.querySelector('.custom-menu-item');
+
+      if (hasTargetPath && !alreadyInjected) {
+        const newItem = document.createElement('div');
+        newItem.className = `${menuItemClass} custom-menu-item`;
+        newItem.role = 'menuitem';
+
+        menu.prepend(newItem);
+
+        const root = createRoot(newItem);
+        root.render(<CreateButton />);
+      }
+    });
   });
 
   observer.observe(document.body, { childList: true, subtree: true });
@@ -67,7 +76,7 @@ function observeMenuOpen() {
 
       if (isAuthenticated)
         root.render(<FolderList />);
-      else 
+      else
         root.render(<AuthPanel />);
 
       observeMenuOpen();
