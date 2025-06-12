cfd0a178d098d56731a54068a8740a416fb59804
Oscar Fernandez <oscarferm13@gmail.com>
2025-06-12 09:48:28 +0200

    Pre-refactor

--- src/content.jsx
+++ src/content.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client'
-import React from 'react'
+import React from 'react';
 import FolderList from './FolderList'
 import CreateButton from './CreateButton';
 import AuthPanel from './AuthPanel';
